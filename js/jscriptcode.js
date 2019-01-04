573d92a1c668fa90707200a701132295b9a7cbce
Josh Dingri <josh@janustechnology.co.uk>
2019-01-04 15:23:21 +0000

    writing education section

---
+++ js/jscriptcode.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      function scrollToAnchor(aid){
+        var aTag = $("a[name='"+ aid +"']");
+        $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+      }
+        $("#aboutlink").click(function() { scrollToAnchor('about-section1');});
+
+        $(function(){
+          $("#aboutme").load("aboutme.html");
+          $("#education").load("education.html");
+        });
+}
